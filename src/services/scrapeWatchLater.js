100b0fc3f7dcb1605000c6c1764ca162b1e8303c
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-11-06 22:21:32 +0200

    init puppeteer with stealth plugin & user login prompt

--- src/services/scrapeWatchLater.js
+++ src/services/scrapeWatchLater.js
@@ -4,3 +4,30 @@ const fs = require('fs');
 const path = require('path');
 const ExcelJS = require('exceljs');
 const cliProgress = require('cli-progress');
+
+puppeteer.use(StealthPlugin());
+
+(async () => {
+	const browser = await puppeteer.launch({
+		headless: false,
+		// slowMo: 200,
+		args: [
+			'--start-maximized',
+			'--window-size=1920,1080',
+			'--no-sandbox',
+			'--disable-setuid-sandbox',
+			'--disable-dev-shm-usage',
+			'--disable-gpu',
+			'--no-first-run',
+		],
+		defaultViewport: null,
+	});
+
+	const [initPage] = await browser.pages();
+	await initPage.goto('https://www.youtube.com', { waitUntil: 'networkidle2' });
+
+	console.log('AUTOMATION PAUSED: Log in manually in the browser, then press Enter in the terminal.');
+
+	await new Promise(resolve => process.stdin.once('data', resolve));
+	await browser.close();
+})();
