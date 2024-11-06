4e171e75ee753472493f6cf0e80daeba855c1f23
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-11-06 20:09:29 +0200

    add initial project structure & dependencies setup

---
+++ src/services/scrapeWatchLater.js
@@ -0,0 +1,6 @@
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const fs = require('fs');
+const path = require('path');
+const ExcelJS = require('exceljs');
+const cliProgress = require('cli-progress');
